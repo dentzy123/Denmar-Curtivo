b88f92b81a3350a1c282752e721c053b766f5be4
dentzy123 <158585763+dentzy123@users.noreply.github.com>
2025-08-04 09:16:35 +0000

    activity1-2 august 4

--- basicdart.dart
+++ basicdart.dart
@@ -1,1 +1,24 @@
-text
+void main() {
+    dynamic name = "Alice";
+    name = 25;
+    print ( "Welcome $name!");
+}
+void main() {
+  double result = calculateArea (2 * 10, 5, false);
+  
+  print('The result for a rectangle is ' + result.toString());
+  result = calculateArea(4, 7, true);
+  print("The Perimeter of the square is  " + result.toString());
+}
+
+double calculateArea(double width,double height, bool isSquare) {
+  double area;
+  if (isSquare){
+    area = width * height * 2;
+  }else {
+     area = width * height;
+  }
+ 
+  return area;
+}
+
